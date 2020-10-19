0e0e05b0fdfe17d94329a2c647107f67d6de75c3
mniudanri <niudanri12@gmail.com>
2020-10-19 20:40:07 +0700

    delete unused tag

--- router/router.go
+++ router/router.go
@@ -11,21 +11,6 @@ import (
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-// @title Echo Swagger Example API
-// @version 1.0
-// @description This is a sample server server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:3000
-// @BasePath /
-// @schemes http
 func InitRoute(port string) {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
